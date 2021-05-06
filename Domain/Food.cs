f651a181cba3ab99d32e56b8dc648fc9cd462bc4
insane_dude <insane.dude.btw@gmail.com>
2021-05-06 11:38:07 +0300

    don't ask...

---
+++ Domain/Food.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public float Weight { get; set; }
+        public float CookingTime { get; set; }
+        public int ChiefId { get; set; }
+    }
+}
