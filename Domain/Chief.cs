33206494cfcb42d89527e4cbbf4edbcd90f9dfce
insane_dude <insane.dude.btw@gmail.com>
2021-05-15 15:54:52 +0300

    redacted file structure and references

--- Domain/Chief.cs
+++ Domain/Chief.cs
@@ -1,11 +1,12 @@
 namespace Domain
 {
-    public abstract class Chief
+    public class Chief
     {
-        public abstract int ChiefLevel { get; set; }
+        public int ChiefLevel { get; set; }
         public int Age { get; set; }
         public string Name { get; set; }
         void Cook(Food food){}
         void GetOrder(){}
+
     }
 }
--- RestaurantProject/MainProgram.cs
+++ RestaurantProject/MainProgram.cs
@@ -1,15 +1,13 @@
 using System;
 using Domain;
+
 namespace RestaurantProject
 {
     public class MainProgram
     {
         static public void Main(string[] args)
         {
-            Chief newChief = new LowLevelChief();
-            Console.WriteLine(newChief.ChiefLevel);
-            Chief newChiefNext = new HighLevelChief();
-            Console.WriteLine(newChiefNext.ChiefLevel);
+            Console.WriteLine("test");
         }
     }
 }
